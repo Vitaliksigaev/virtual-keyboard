c1c5a55c974f858c86248e04ad67f7cc6cb65c29
iMac <imac@iMac-iMac.local>
2023-04-29 20:27:25 +0300

    feat: add button and addEvent on btn

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let body = document.querySelector('body');
 
 let textarea = document.createElement('div');
 textarea.className = 'textarea';
-textarea.innerHTML = '<input type="text" size="40">';
+textarea.innerHTML = '<input class="text" type="text" autofocus>';
 body.append(textarea)
 
 
@@ -13,10 +13,10 @@ body.append(keyboard)
 
 let keyboardKeys = [
     [ '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=','Backspace'],
-    ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\|'],
-    ['Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '"', 'Enter'],
+    ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\'],
+    ['CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', `'`, 'Enter'],
     ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'Shift'],
-    ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Win', 'Fn', 'Ctrl']
+    ['Control', 'Meta', 'Alt', 'Space', 'Alt', 'Meta', 'ContextMenu', 'Control']
 ]
 
 for( let i = 0; i < keyboardKeys.length; i++ ) {
@@ -29,15 +29,39 @@ for( let i = 0; i < keyboardKeys.length; i++ ) {
         let btn = document.createElement('div');
         btn.className = 'keys';
         btn.innerText = keyboardKeys[i][j];
-        btn.addEventListener('click', function() {
-            console.log(keyboardKeys[i][j]);
-        })
+        btn.setAttribute('keyname', keyboardKeys[i][j]);
+        btn.setAttribute('lowerCaseName', keyboardKeys[i][j].toLowerCase() )
+        // btn.addEventListener('click', function() {
+        //     console.log(keyboardKeys[i][j]);
+        //     if(e.keys == keyboardKeys[i][j].getAttribute('keyname')) {
+        //         console.log(keys)
+        //     } 
+        // })    
+
         row.append(btn)
     }
 }
 
-function createRow() {
-    let row = document.createElement('div');
-    row.className = 'row';
-    return row
-}
+let keys = document.querySelectorAll('.keys');
+console.log(keys)
+
+// function createRow() {
+//     let row = document.createElement('div');
+//     row.className = 'row';
+//     return row
+// }
+
+
+document.addEventListener('keydown', function(e){
+    for( let i = 0; i < keys.length; i++) {
+        if(e.key == keys[i].getAttribute('keyname') || e.key == keys[i].getAttribute('lowerCaseName')) {
+            console.log(keys[i]);
+            keys[i].classList.toggle('btn-animated');
+        }
+    }
+    
+    console.log(keyboardKeys);
+    console.log(e.key);
+
+    
+})
